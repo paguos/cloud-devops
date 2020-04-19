d439744589ecebf0beb2eb1aee9c01463296055c
Pablo Osinaga <paguos@gmail.com>
2020-04-19 15:46:36 +0200

    feat: complete upload_docker.sh

--- ml-k8s/upload_docker.sh
+++ ml-k8s/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=paguos/udacity-ml-k8s:v0.1.0
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml-k8s $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
